97409e7efb167dbf73a50a4e1e1166cfa5f4a18f
caleb-ali <slimkally@gmail.com>
2022-10-18 23:55:32 +0100

    little change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,62 @@
-import React, {useEffect} from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
-import { FiShoppingCart } from 'react-icons/fi';
-import { BsChatLeft } from 'react-icons/bs';
-import { RiNotification3Line } from 'react-icons/ri';
-import { MdKeyboardArrowDown } from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import avatar from '../data/avatar.jpg';
-import {Cart, Chat, Notification, UserProfile, } from '.';
-import { useStateContext } from '../contexts/ContextProvider';
+import React, { useEffect } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import avatar from "../data/avatar.jpg";
+import { Cart, Chat, Notification, UserProfile } from ".";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
-  <TooltipComponent content={title} position= "BottomCenter">
-      <button type="button" onClick={customFunc} style={{ color}} className="relative text-xl rounded-full p-3 hover:bg-light-gray">
-        <span style={{background: dotColor}} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-          {icon}
-        </span>
-      </button>
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      type="button"
+      onClick={customFunc}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      >
+        {icon}
+      </span>
+    </button>
   </TooltipComponent>
-)
-
+);
 
 const NavBar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const { activeMenu, setActiveMenu } = useStateContext();
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
-      <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu/>} />
+      <NavButton
+        title="Menu"
+        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
 
       <div className="flex">
-      <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu/>} />
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick("Cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+
+        <NavButton
+          title="Chat"
+          dotColor="#"
+          customFunc={() => handleClick("Chat")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
